e98e2caa2b8b82abb3bd49ad3d8af6aa5b9cbfb6
hassankoroma <stomata-studded-7f@iclound.com>
2025-05-20 20:28:05 +0900

    first commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,19 +1,25 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoJDBCImpl nameDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
+        nameDao .createUsersTable();
 
     }
 
     public void dropUsersTable() {
+        nameDao.dropUsersTable();
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        nameDao.saveUser(name, lastName, age);
 
     }
 
